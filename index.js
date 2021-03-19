202dd3a8493c53e5e84b2c529e79c72138f56c51
unknown <jpchu@up.edu.ph>
2021-03-19 20:24:48 +0800

    added database

--- index.js
+++ index.js
@@ -1,88 +1,130 @@
-const { request } = require('express');
+require('dotenv').config()
+
+const Note = require('./models/note')
 const express = require('express')
 const cors = require('cors')
 
+
 const app = express();
 
+app.use(express.static('build'))
+app.use(express.json())
 
 app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       date: "2019-05-30T17:30:31.098Z",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only Javascript",
+//       date: "2019-05-30T18:39:34.091Z",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       date: "2019-05-30T19:20:14.298Z",
+//       important: true
+//     }
+//   ]
 
 app.get('/', (req,res) =>{
-    res.send('<h1> Hello  Joshua</h1>')
-})
-
-app.get('/api/notes/:id', (req,res)=>{
-    const id = Number(req.params.id)
-    const note = notes.find(n => n.id === id)
-
-
-    if(note){
-      res.json(note)
-    }else{
-      res.send('<h1> This page does not exist</h1>')
-    }
 })
 
 
 app.get('/api/notes', (req,res)=>{
-  res.json(notes)
+  Note.find({}).then(notes=>{
+    console.log("fetched data")
+    res.json(notes)
+  })
 })
+
+
+
+app.get('/api/notes/:id', (req,res, next)=>{
+    Note.findById(req.params.id).then(note =>{
+      if(note){
+        res.json(note)
+      }else{
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+    // if(note){
+    //   res.json(note)
+    // }else{
+    //   res.send('<h1> This page does not exist</h1>')
+    // }
+})
+
+
 
 app.delete('/api/notes/:id', (req,res)=>{
-  const id = Number(req.params.id)
-  notes = notes.filter(note=> note.id !== id)
 
-  res.status(204).end()
+  Note.findByIdAndRemove(req.params.id)
+    .then(result =>{
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/notes', (req,res)=>{
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) 
-    : 0
+app.post('/api/notes', (req,res, next)=>{
 
-  const note = req.body
-  note.id = maxId + 1
+  const body = req.body
 
-  notes = notes.concat(note)
 
-  res.json(note)
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save().then(savedNote => {
+    console.log(savedNote)
+    res.json(savedNote)
+  })
+  .catch(error => next(error))
 })
 
 
 
 app.put('/api/notes/:id', (req,res)=>{
-  const id = Number(req.params.id)
-  const changedNote = req.body
+  const body = req.body
   // const note = notes.find(n => n.id === id)
   // const  changedNote = {...note, important: !note.important}
   
-  notes = notes.map(n => n.id !== id ? n : changedNote)
-  res.json(changedNote)
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, {new: true})
+    .then(updatedNote =>{
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
+
+const errorHandler = (error, req,res,next) =>{
+  console.log(error.message)
+
+  if (error.name === 'CastError'){
+    return res.status(400).send({error: "malformatted id"})
+  }else if(error.name === 'ValidationError'){
+    return res.status(400).json({error: error.message})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
